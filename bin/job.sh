955f2b7070b37ed62722f22ece2dac4266745a3c
ja-slater <72303408+ja-slater@users.noreply.github.com>
2020-10-05 19:57:21 +0100

    Add files via upload

---
+++ bin/job.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#PBS -N 2611
+#PBS -l select=38
+#PBS -A n02-chem
+#PBS -l walltime=24:00:00
+
+export MPICH_ENV_DISPLAY=1
+cd $PBS_O_WORKDIR
+
+aprun -n 900 ./les.mpi | tee uclales-salsa.output
+
+set -ex
